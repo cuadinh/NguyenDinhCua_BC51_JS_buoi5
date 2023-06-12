9107104873ebc7338fe0c5c4af18fcfde2ef2d90
cuadinhnguyen <dinhcua89@gmail.com>
2023-06-12 07:01:38 +0700

    update baiTap 2

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 // BAI 1 QUAN LY SINH VIEN
+// 
+//  DOI_TUONG_1 = 2.5;
+//  DOI_TUONG_2 = 1.5;
+//  DOI_TUONG_3 = 1;
 
+//  KHU_VUC_A = 2;
+//  KHU_VUC_B = 1;
+//  KHU_VUC_C = 0.5;
+
+// KHAI BAO BIEN
 
 var diemMon_1 = 0;
 var diemMon_2 = 0;
@@ -21,6 +30,7 @@ getEle("btnResult").onclick = function () {
 
   diemTongKet = diemMon_1 + diemMon_2 + diemMon_3 + diemKhuVuc + diemDoiTuong;
 
+  //    console.log(diemKhuVuc);
   if (diemMon_1 === 0 || diemMon_2 === 0 || diemMon_3 === 0 || diemTongKet<diemChuan) {
     getEle("txtResult").innerHTML = "Kết quả: Bạn đã rớt";
   } else {
@@ -28,3 +38,48 @@ getEle("btnResult").onclick = function () {
 
   }
 };
+
+
+//BAI 2: TÍNH TIỀN ĐIỆN
+/**
+ * Viết chương trình nhập vào thông tin tiêu thụ điện 
+ * Tinh và xuất tiền trả theo quy tắc:
+ * 50kw đầu : 500d/kw
+ * 50kw kế : 650d/kw
+ * 100kw kế : 850d/kw
+ * 150kw kế : 1100d/kw
+ * còn lại: 1300d/kw
+ * 
+ */
+
+var KM_50 = 500;
+var KM_100 = 650;
+var KM_200 = 850;
+var KM_350 = 1100;
+var KM_CON_LAI = 1300;
+var tongTienDien = 0;
+
+
+getEle("btnElecBill").onclick = function(){
+    var hoTen = getEle("inputName").value;
+    var SoKm = Number(getEle("inputKW").value);
+    if(SoKm>0 && SoKm <=50){
+        tongTienDien = SoKm * KM_50;
+    }else if ( SoKm >50 && SoKm <=100){
+        tongTienDien = 50 * KM_50 + (SoKm - 50) * KM_100;
+
+    }else if (SoKm >100 && SoKm <=200){
+        tongTienDien = 50 * KM_50 + 50 * KM_100 + (SoKm - 100)*KM_200;
+    }else if (SoKm >200 && SoKm <=350){
+        tongTienDien = 50 * KM_50 + 50 * KM_100 + 100*KM_200 + (SoKm - 200) * KM_350;
+
+    }else if (SoKm >350){
+        tongTienDien = 50 * KM_50 + 50 * KM_100 + 100*KM_200 + 150 * KM_350 + (SoKm - 350)*KM_CON_LAI;
+        console.log(tongTienDien);
+
+    }
+
+    getEle("txtElecBill").innerHTML = "Tên Khách Hàng: " + hoTen + " - " + "Tổng tiền điện: " + tongTienDien;
+
+}
+
